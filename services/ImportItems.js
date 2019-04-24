9405abd709db3293164671d440eb627a1153a51c
joe beuckman <joebeuckman@gmail.com>
2019-04-24 22:17:18 +0200

    queue then process each item in import

--- services/ImportItems.js
+++ services/ImportItems.js
@@ -1,6 +1,40 @@
 "use strict";
 const request = require("request");
 const contentTypeParser = require("content-type-parser");
+const fileUtils = require("./utils/fileUtils");
+
+const queues = {};
+
+const IMPORT_THROTTLE = 1000;
+
+const importNextItem = importConfig => {
+  const item = queues[importConfig.id].shift();
+  if (!item) {
+    console.log("import complete", importConfig);
+    importConfig.ongoing = false;
+
+    strapi.plugins["import-content"].models["importconfig"]
+      .forge(importConfig)
+      .save();
+    return;
+  }
+
+  console.log("will import", item);
+
+  //TODO: map fields and create target model
+
+  strapi.plugins["import-content"].models["importeditem"]
+    .forge({ importconfig: importConfig.id })
+    .save();
+
+  importConfig.progress++;
+
+  strapi.plugins["import-content"].models["importconfig"]
+    .forge(importConfig)
+    .save();
+
+  setTimeout(() => importNextItem(importConfig), IMPORT_THROTTLE);
+};
 
 module.exports = {
   importItems: importConfig =>
@@ -8,27 +42,25 @@ module.exports = {
       const importConfigRecord = importConfig.attributes;
       console.log("importitems", importConfigRecord);
 
-      const url = importConfig.attributes.url;
+      const url = importConfigRecord.url;
 
       const { contentType, body } = await strapi.plugins[
         "import-content"
       ].services["filedataresolver"].getDataFromUrl(url);
+
+      const { items } = await fileUtils.getItemsForFileData({
+        contentType,
+        body
+      });
+      console.log({ items });
+      queues[importConfigRecord.id] = items;
+      console.log({ queueItems: queues[importConfigRecord.id] });
 
       resolve({
         status: "import started",
         importConfigId: importConfigRecord.id
       });
 
-      setInterval(() => {
-        strapi.plugins["import-content"].models["importeditem"]
-          .forge({ importconfig: importConfigRecord.id })
-          .save();
-
-        importConfigRecord.progress++;
-
-        strapi.plugins["import-content"].models["importconfig"]
-          .forge(importConfigRecord)
-          .save();
-      }, 1000);
+      importNextItem(importConfigRecord);
     })
 };
